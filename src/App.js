f77236f6926b4f8334cdb8165be965b27596d896
Nikhil Bedarkar <bedarkar39@gmail.com>
2022-08-17 15:57:39 +0530

    deployment changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import React,{useState,useEffect} from 'react';
 import Coin from './Coin';
 
 function App() {
+  const url="https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false";
   const [coins,setCoins]=useState([]);
   const [search,setSearch]=useState('');
   useEffect(()=>{
-    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false").then(res=>{
+    axios.get(url).then(res=>{
       setCoins(res.data);
     }).catch(error => console.log(error))
   });
